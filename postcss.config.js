6e7f4f02e3e118afbb22cf6023db8cd43118afd4
David Winterbottom <david.winterbottom@gmail.com>
2022-10-17 21:21:24 +0100

    Add basic styles with Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {}
-    }
+    plugins: [
+        // Allow use of @import statements
+        // https://tailwindcss.com/docs/using-with-preprocessors#build-time-imports
+        require('postcss-import'),
+        // Allow nested declarations
+        // https://tailwindcss.com/docs/using-with-preprocessors#nesting
+        require('tailwindcss/nesting'),
+        // Use Tailwind
+        require('tailwindcss'),
+        // Automatically manage vendor prefixes
+        // https://tailwindcss.com/docs/using-with-preprocessors#vendor-prefixes
+        require('autoprefixer'),
+    ]
 }
